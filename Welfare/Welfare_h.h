f41eb0ea3f9a5e1a5240b144bc07d0671afd8af5
shallots <shallots@163.com>
2015-05-27 13:10:06 +0800

    delete tag1.1.1.3

--- Welfare/Welfare_h.h
+++ Welfare/Welfare_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sun Nov 02 20:26:54 2014
+/* at Wed May 27 13:05:53 2015
  */
 /* Compiler settings for Welfare.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
